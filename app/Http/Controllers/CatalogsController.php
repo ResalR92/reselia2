a5c3f3ab6b9cc8b6ee22a5056c387fb08f820352
ResalR92 <resalramdahadi92@gmail.com>
2017-03-31 18:53:39 +0700

    catalog->search->product

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -12,16 +12,20 @@ class CatalogsController extends Controller
 {
     public function index(Request $request)
     {
+    	$q = $request->get('q');
     	if($request->has('cat')){
     		$cat = $request->get('cat');
     		$category = Category::findOrFail($cat);
 
     		// we use this to get product from current category and its child
-    		$products = Product::whereIn('id',$category->related_products_id)->paginate(4);
+    		$products = Product::whereIn('id',$category->related_products_id)
+    							->where('name','LIKE','%'.$q.'%');
     	} else{
-    		$products = Product::paginate(4);
+    		$products = Product::where('name','LIKE','%'.$q.'%');
     	}
 
-    	return view('catalogs.index',compact('products', 'cat', 'category'));
+    	$products = $products->paginate(4);
+
+    	return view('catalogs.index',compact('products', 'cat', 'category','q'));
     }
 }
--- resources/views/catalogs/index.blade.php
+++ resources/views/catalogs/index.blade.php
@@ -4,6 +4,11 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
+				@include('catalogs._search-panel',[
+					'q' => isset($q) ? $q : null,
+					'cat' => isset($cat) ? $cat : ''
+				])
+
 				@include('catalogs._category-panel')
 
 				@if (isset($category) && $category->hasChild())
@@ -19,14 +24,37 @@
 					<div class="col-md-12">
 						@include('catalogs._breadcrumb',['current_category'=>isset($category) ? $category : null])
 					</div>
-					@foreach ($products as $product)
+					@forelse($products as $product)
 						<div class="col-md-6">
 							@include('catalogs._product-thumnail',['product'=>$product])
 						</div>
-					@endforeach
+						@empty
+							<div class="col-md-12 text-center">
+								@if (isset($q))
+									<h1>:(</h1>
+									<p>Produk yang kamu cari tidak ditemukan.</p>
+									@if (isset($category))
+										<p>
+											<a href="{{ url('/catalogs?='.$q) }}">
+												Cari di semua kategori <i class="fa fa-arrow-right"></i>
+											</a>
+										</p>
+									@else
+										<h1>:|</h1>
+										<p>Belum ada produk untuk kategori ini.</p>
+									@endif
+
+									<p>
+										<a href="{{ url('/catalogs') }}">
+											Lihat semua produk <i class="fa fa-arrow-right"></i>
+										</a>
+									</p>
+								@endif
+							</div>
+					@endforelse
 
 					<div class="pull-right">
-						{!! $products->appends(compact('cat'))->links() !!}
+						{!! $products->appends(compact('cat','q'))->links() !!}
 					</div>
 				</div>
 			</div>
