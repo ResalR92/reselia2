526c0ca254956052b21d881d6fb2e0733023d6b0
ResalR92 <resalramdahadi92@gmail.com>
2017-04-03 10:42:14 +0700

    cart->db->cookie->merge()

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,9 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use App\Support\CartService;
 
 class Authenticate
 {
+    protected $cart;
+
+    public function __construct(CartService $cart)
+    {
+        $this->cart = $cart;
+    }
     /**
      * Handle an incoming request.
      *
@@ -23,6 +30,14 @@ class Authenticate
             } else {
                 return redirect()->guest('login');
             }
+        }
+
+        if($request->user()->can('customer-access')){
+            //merge cart from cookie to db
+            //send response while remove cart from cookie
+            $cookie = $this->cart->merge();
+
+            return $next($request)->withCookie($cookie);
         }
 
         return $next($request);
